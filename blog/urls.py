3805142c55e2ed304171e8e9d9bac314c81ed6d0
Adam Thompson <adam@adamth.com>
2018-04-17 18:06:51 +1000

    Add users and authentication

--- blog/urls.py
+++ blog/urls.py
@@ -2,8 +2,11 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.post_list, name='post_list'),
+    url(r'^$', views.post_list, name='index'),
     url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
     url(r'^post/new/$', views.post_new, name='post_new'),
     url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
+    url(r'^register/$', views.register, name='register'),
+    url(r'^signin/', views.sign_in, name='sign_in'),
+    url(r'^signout/', views.sign_out, name='sign_out'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from .models import Post
 from .forms import PostForm
 
@@ -7,12 +9,12 @@ from .forms import PostForm
 def post_list(request):
     posts = Post.objects.filter(
         published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {'posts': posts})
+    return render(request, 'blog/post/list.html', {'posts': posts})
 
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html', {'post': post})
+    return render(request, 'blog/post/detail.html', {'post': post})
 
 
 def post_new(request):
@@ -26,7 +28,7 @@ def post_new(request):
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm()
-    return render(request, 'blog/post_edit.html', {'form': form})
+    return render(request, 'blog/post/edit.html', {'form': form})
 
 
 def post_edit(request, pk):
@@ -38,7 +40,45 @@ def post_edit(request, pk):
             post.author = request.user
             post.published_date = timezone.now()
             post.save()
-            return redirect('post_detail', pk=post.pk)
+            return redirect('post/detail', pk=post.pk)
     else:
         form = PostForm(instance=post)
-    return render(request, 'blog/post_edit.html', {'form': form})
+    return render(request, 'blog/post/edit.html', {'form': form})
+
+
+def register(request):
+    if request.method == 'POST':
+        # Get the user input from the form
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            # Save the user if the form is valid
+            form.save()
+            # Get the users credentials to log them in and redirect to the home page
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('index')
+    else:
+        form = UserCreationForm()
+    return render(request, 'blog/registration/register.html', {'form': form})
+
+
+def sign_in(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=raw_password)
+            if user is not None:
+                login(request, user)
+                return redirect('index')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'blog/registration/signin.html', {'form': form})
+
+
+def sign_out(request):
+    logout(request)
+    return redirect('index')
