873661991fbbf3cad77b493114ea09af3303aca3
Adam Thompson <adam@adamth.com>
2018-04-18 17:52:09 +1000

    Remove pypug

--- mysite/settings.py
+++ mysite/settings.py
@@ -55,23 +55,14 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates/portfolio/'],
+        'DIRS': [],
+        'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-            ],
-            'loaders': [
-                # PyPugJS part:   ##############################
-                ('pypugjs.ext.django.Loader', (
-                    'django.template.loaders.filesystem.Loader',
-                    'django.template.loaders.app_directories.Loader',
-                ))
-            ],
-            'builtins': [
-                'pypugjs.ext.django.templatetags',
             ],
         },
     },
